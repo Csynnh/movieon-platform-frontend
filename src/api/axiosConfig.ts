f99c929313fecef087926221f9549dbf7587f30f
Csynnh <congsinhv@gmail.com>
2024-05-20 10:48:08 +0700

    chore: Update axiosConfig.ts to use environment variable for base URL

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create();
 
-instance.defaults.baseURL = "http://localhost:1217/";
+// api in env
+instance.defaults.baseURL = process.env.REACT_APP_API_GATEWAY_URL;
 
 export default instance;
